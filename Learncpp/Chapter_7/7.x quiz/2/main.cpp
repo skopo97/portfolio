788a154edf7729c61b4f5b701d1b923a568b8f6a
sami <samikoponen4@gmail.com>
2024-10-18 16:27:00 +0300

    .

---
+++ Learncpp/Chapter_7/7.x quiz/2/main.cpp
@@ -0,0 +1,20 @@
+#include "constants.h"
+#include <iostream>
+
+int main()
+{
+    std::cout << "how many students are in your class? ";
+    int students{};
+    std::cin >> students;
+
+    if (students > Constants::maxClassSize)
+    {
+        std::cout << "There are too many students in this class";
+    }
+    else
+    {
+        std::cout << "This class isn't too large";
+    }
+
+    return 0;
+}
