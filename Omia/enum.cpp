587785c3d4ed427bf687eeae844a329356a06882
skopo97 <samikoponen4@gmail.com>
2024-10-30 20:01:52 +0200

    enumia

--- Omia/enum.cpp
+++ Omia/enum.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <string_view>
-namespace monster{
-    enum class monsterType{
+
+namespace monster
+{
+    enum class monsterType
+    {
         orc,
         goblin,
         troll,
@@ -11,32 +14,39 @@ namespace monster{
     };
 }
 
-constexpr std::string_view getMonsterType(monster::monsterType Type){
+constexpr std::string_view getMonsterType(monster::monsterType Type)
+{
 
     using enum monster::monsterType;
 
-    switch(Type){
-        case orc: return "orc";
-        case goblin: return "goblin";
-        case troll: return "troll";
-        case ogre: return "ogre";
-        case skeleton: return "skeleton";
-        default: return "???";
-
+    switch (Type)
+    {
+    case orc:
+        return "orc";
+    case goblin:
+        return "goblin";
+    case troll:
+        return "troll";
+    case ogre:
+        return "ogre";
+    case skeleton:
+        return "skeleton";
+    default:
+        return "???";
     }
-
-
 }
 
-//Overload << operator for monsterType
+// Overload << operator for monsterType
 
-std::ostream& operator<<(std::ostream& out, monster::monsterType Type){
+std::ostream &operator<<(std::ostream &out, monster::monsterType Type)
+{
     out << getMonsterType(Type);
     return out;
 }
 
 int main()
 {
+
     constexpr monster::monsterType monster{monster::monsterType::orc};
     std::cout << "The monster is a " << monster << '\n';
 
