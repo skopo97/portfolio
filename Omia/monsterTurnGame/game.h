3a75ddc0db9fbeec5556eabaf99ba928245f45f9
skopo97 <samikoponen4@gmail.com>
2024-11-04 22:51:51 +0200

    game function declarations

--- Omia/monsterTurnGame/game.h
+++ Omia/monsterTurnGame/game.h
@@ -1,6 +1,12 @@
 #ifndef GAME_H
 #define GAME_H
+#include "monsters.h"
 
 // Your declarations here
+
+void chooseYourMonster(monster &chosenMonster);
+void chooseYourEnemyMonster(monster &enemyMonster);
+
+void attack(monster &attacker, monster &defender);
 
 #endif // GAME_H
