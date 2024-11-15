2bd6faf06b84073b999585670d384bb87d98cfc9
skopo97 <samikoponen4@gmail.com>
2024-11-15 16:57:38 +0200

    all fixed

--- Omia/monsterTurnGame/monsters.h
+++ Omia/monsterTurnGame/monsters.h
@@ -1,5 +1,6 @@
 #ifndef MONSTERS_H
 #define MONSTERS_H
+#include <ostream>
 #include <string>
 
 // Your code here
@@ -12,6 +13,8 @@ class monster {
   int hp{};
   int attack{};
   int defense{};
+
+  std::string s = "Hey";
 };
 
 std::string getMonsterType(MonsterType type);
